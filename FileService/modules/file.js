351957798f79018822ed00cbd6d8cc852a0bcbe8
harri361 <harri361@uni.coventry.ac.uk>
2019-11-14 21:48:08 +0000

    Fixed complexity 8 issue

--- FileService/modules/file.js
+++ FileService/modules/file.js
@@ -12,9 +12,9 @@ module.exports = class File {
 
 	async uploadFile(path, fileName, user) {
 		try {
-			if(path === undefined || path.length === 0) throw new Error('file must have a path')
-			if(fileName === undefined || fileName.length === 0) throw new Error('file must have a filename')
-			if(user === undefined || user.length === 0) throw new Error('file must have a user')
+			this.checkPath(path)
+			this.checkFileName(fileName)
+			this.checkUser(user)
 
 			await fs.copy(path, `files/${user}/${fileName}`)
 			return true
@@ -22,5 +22,20 @@ module.exports = class File {
 		} catch(err) {
 			throw err
 		}
+	}
+
+	checkPath(path) {
+		if(path === undefined || path.length === 0 || path === null)
+			throw new Error('file must have a path')
+	}
+
+	checkFileName(fileName) {
+		if(fileName === undefined || fileName.length === 0 || fileName === null)
+			throw new Error('file must have a filename')
+	}
+
+	checkUser(user) {
+		if(user === undefined || user.length === 0 || user === null)
+			throw new Error('file must have a user')
 	}
 }
